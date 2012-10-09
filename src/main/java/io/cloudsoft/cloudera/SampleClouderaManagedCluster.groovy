9bd9748c9056d9b56a8964d5255d54f5536ee23e
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2012-10-09 11:12:20 +0100

    use new launcher API

--- src/main/java/io/cloudsoft/cloudera/SampleClouderaManagedCluster.groovy
+++ src/main/java/io/cloudsoft/cloudera/SampleClouderaManagedCluster.groovy
@@ -89,14 +89,17 @@ public class SampleClouderaManagedCluster extends AbstractApplication {
         
     public static void main(String[] argv) {
         ArrayList args = new ArrayList(Arrays.asList(argv));
-        int port = CommandLineUtil.getCommandLineOptionInt(args, "--port", 8085);
-        List<Location> locations = new LocationRegistry().getLocationsById(args ?: [DEFAULT_LOCATION])
-
         SampleClouderaManagedCluster app = new SampleClouderaManagedCluster(name:'Brooklyn Cloudera Managed Cluster');
             
         try {
-            BrooklynLauncher.manage(app, port)
+            BrooklynLauncher.newLauncher().managing(app).
+                webconsolePort(CommandLineUtil.getCommandLineOption(args, "--port", "8081+")).
+                launch();
+                
+            List<Location> locations = new LocationRegistry().getLocationsById(args ?: [DEFAULT_LOCATION])
+            
             app.start(locations)
+            
         } catch (Throwable t) {
             log.warn("FAILED TO START "+app+": "+t, t);
         }
