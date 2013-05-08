5a113328cc28376b8cc06291c898df9c6fb0c0a5
Aled Sage <aled.sage@gmail.com>
2013-05-08 12:58:38 +0100

    Convert entity interfaces from groovy to java

---
+++ src/main/java/io/cloudsoft/cloudera/brooklynnodes/AllServices.java
@@ -0,0 +1,14 @@
+package io.cloudsoft.cloudera.brooklynnodes;
+
+import brooklyn.entity.Effector;
+import brooklyn.entity.basic.MethodEffector;
+import brooklyn.entity.proxying.ImplementedBy;
+
+@ImplementedBy(AllServicesImpl.class)
+public interface AllServices extends StartupGroup {
+
+    public static final Effector<String> COLLECT_METRICS = new MethodEffector<String>(AllServices.class, "collectMetrics");
+
+    public String collectMetrics();
+    
+}
--- src/main/java/io/cloudsoft/cloudera/brooklynnodes/ClouderaCdhNodeDriver.java
+++ src/main/java/io/cloudsoft/cloudera/brooklynnodes/ClouderaCdhNodeDriver.java
@@ -1,6 +1,6 @@
 package io.cloudsoft.cloudera.brooklynnodes;
 
-import brooklyn.entity.basic.SoftwareProcessDriver
+import brooklyn.entity.basic.SoftwareProcessDriver;
 
 public interface ClouderaCdhNodeDriver extends SoftwareProcessDriver {
 
