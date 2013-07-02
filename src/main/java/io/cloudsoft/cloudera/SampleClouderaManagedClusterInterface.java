89dfa4bb138cf0c65dd92da7b5d2d0fbb42f4010
Aled Sage <aled.sage@gmail.com>
2013-07-02 16:54:03 +0100

    Use internal DNS
    - as first step, using GeoScaling.com
    - as entity comes up/down, add to dns

--- src/main/java/io/cloudsoft/cloudera/SampleClouderaManagedClusterInterface.java
+++ src/main/java/io/cloudsoft/cloudera/SampleClouderaManagedClusterInterface.java
@@ -1,12 +1,16 @@
 package io.cloudsoft.cloudera;
 
 import io.cloudsoft.cloudera.brooklynnodes.ClouderaManagerNode;
+import brooklyn.config.ConfigKey;
+import brooklyn.entity.basic.ConfigKeys;
 import brooklyn.entity.basic.StartableApplication;
 import brooklyn.entity.proxying.ImplementedBy;
 import brooklyn.event.AttributeSensor;
 
 @ImplementedBy(SampleClouderaManagedCluster.class)
 public interface SampleClouderaManagedClusterInterface extends StartableApplication {
+
+    public static final ConfigKey<Boolean> SETUP_DNS = ConfigKeys.newBooleanConfigKey("cdh.setupDns", "Whether to set up internal DNS", true);
 
     public static final AttributeSensor<String> CLOUDERA_MANAGER_URL = ClouderaManagerNode.CLOUDERA_MANAGER_URL;
     
